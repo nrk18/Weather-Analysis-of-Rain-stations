7a69e1ac0569172a1e5f5b6af2995b4629d735c9
nrk18 <84769035+nrk18@users.noreply.github.com>
2022-09-16 13:48:59 +0530

    Update query-b.sql

--- query-b.sql
+++ query-b.sql
@@ -1,1 +1,3 @@
-SELECT AVG(`pm 2.5`) AS `Average PM 2.5`,AVG(`vpm 2.5`) AS `Average VPM 2.5`,`location` AS `Location` FROM `readings`,`stations` WHERE (CAST(`date time` AS TIME)> '07:30:00' AND CAST(`date time` AS TIME)< '08:30:00') AND YEAR(`date time`)=2019 AND `readings`.`SiteID`=`stations`.`SiteID` GROUP BY `location`;
+SELECT AVG(`pm 2.5`) AS `Average PM 2.5`,AVG(`vpm 2.5`) AS `Average VPM 2.5`,`location` 
+AS `Location` FROM `readings`,`stations` WHERE (CAST(`date time` AS TIME)> '07:30:00' AND CAST(`date time` AS TIME)< '08:30:00') 
+AND YEAR(`date time`)=2019 AND `readings`.`SiteID`=`stations`.`SiteID` GROUP BY `location`;
