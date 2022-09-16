e143a711e65c1d60d16669f735bfa79d028522ba
nrk18 <84769035+nrk18@users.noreply.github.com>
2022-09-16 13:48:38 +0530

    Update query-a.sql

--- query-a.sql
+++ query-a.sql
@@ -1,1 +1,3 @@
-SELECT `date time` as `Date and Time`,`location` AS `Location`,`nox` AS `Max NOx` FROM `readings`,`stations` WHERE `nox`= (SELECT MAX(`nox`) FROM `readings` WHERE YEAR(`date time`)=2019) AND `readings`.`SiteID`=`stations`.`SiteID`;
+SELECT `date time` as `Date and Time`,`location` AS `Location`,`nox` AS `Max NOx` 
+FROM `readings`,`stations` WHERE `nox`= (SELECT MAX(`nox`) FROM `readings` 
+                                         WHERE YEAR(`date time`)=2019) AND `readings`.`SiteID`=`stations`.`SiteID`;
